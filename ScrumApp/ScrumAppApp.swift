40369dfc16f49e78374c4f4614622a2dc66c6734
Andy L <36180382+Andylochan@users.noreply.github.com>
2021-02-26 21:34:08 -0500

    Initial Commit

---
+++ ScrumApp/ScrumAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumAppApp.swift
+//  ScrumApp
+//
+//  Created by Andy Lochan on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
