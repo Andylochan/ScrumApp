4cef82a0587a516551372a76f7a1a4cd6f3cb137
Andy L <36180382+Andylochan@users.noreply.github.com>
2021-02-28 17:43:37 -0500

    Add Binding

--- ScrumApp/Views/DetailView.swift
+++ ScrumApp/Views/DetailView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct DetailView: View {
+    @Binding var scrum: DailyScrum
+    @State private var data: DailyScrum.Data = DailyScrum.Data() 
     @State private var isPresented = false
-    let scrum: DailyScrum
     
     var body: some View {
         List {
@@ -46,16 +47,18 @@ struct DetailView: View {
         .listStyle(InsetGroupedListStyle())
         .navigationBarItems(trailing: Button("Edit") {
             isPresented = true
+            data = scrum.data
         })
         .navigationTitle(scrum.title)
         .fullScreenCover(isPresented: $isPresented) {
             NavigationView {
-                EditView()
+                EditView(scrumData: $data)
                     .navigationTitle(scrum.title)
                     .navigationBarItems(leading: Button("Cancel") {
                         isPresented = false
                     }, trailing: Button("Done") {
                         isPresented = false
+                        scrum.update(from: data)
                     })
             }
         }
@@ -65,7 +68,7 @@ struct DetailView: View {
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            DetailView(scrum: DailyScrum.data[0])
+            DetailView(scrum: .constant(DailyScrum.data[0]))
         }
     }
 }
